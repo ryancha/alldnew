96ef0c531482c2681a3bca2c8176c5f9929608e9
Ikhwan <of.ryancha@gmail.com>
2018-09-29 06:59:39 +0700

    Update post.js

--- materialblog/post.js
+++ materialblog/post.js
@@ -1,36 +1,42 @@
-$(document)['ready'](function() {
-    var _0x4f83x1 = $('a.blog-pager-older-link')['attr']('href');
-    $('a.blog-pager-older-link')['load'](_0x4f83x1 + ' .post-title:first', function() {
-        var _0x4f83x1 = $('a.blog-pager-older-link')['text']();
-        $('a.blog-pager-older-link')['text'](_0x4f83x1 + '\xBB')
+'use strict';
+/** @type {!Array} */
+var _0xaede = ["href", "attr", "a.blog-pager-older-link", " .post-title:first", "text", "\u00bb", "load", "a.blog-pager-newer-link", "a.blog-pager-newer-link:first", "\u00ab", "ready", "#HTML1", "enc", "bismillah", "decrypt", "AES", ",", "split", "location", ".", "lastIndexOf", "substring", "dihak", "indexOf", "#sidebar-atas", "insertBefore", '<div class="ads" style="margin-bottom: 20px;border-radius: 5px;overflow: hidden;box-shadow: 0 3px 1px rgba(0,0,0,0.16),0 3px 6px rgba(0,0,0,0.23);display: inherit;height: 250px;"><a href="http://www.bloggerku.com/" target="_blank" title="Kumpulan Tutorial Blogger"><img alt="bloggerku" src="https://3.bp.blogspot.com/-6kCkIiT4sRo/Vy_t7jexLCI/AAAAAAAAAY0/S4FUZ4w3vPAHbVirMuT9I2W2RlpKV8iAACLcB/s1600/iklan%2Bbloggerku.png"></a><span class="info-iklan" style="display: none;background:#d3d3d3;width:100px;height:15px;border-bottom-left-radius:4px;position:absolute;top:0;right:0;color:#000;font:normal 11px Arial,Sans-Serif;text-align:left;overflow:hidden;padding-right:19px;display: none;">Iklan oleh Bloggerku</span><span class="info-icon" style="width:15px;height:15px;position:absolute;top:0;right:0;cursor:pointer"><a href="http://www.bloggerku.com/p/pasang-iklan.html" target="_blank"><img alt="info" src="http://2.bp.blogspot.com/--ivaHIgXThk/UnJWU80FLhI/AAAAAAAAF_Y/WrH-8aYijGw/s1600/info-iklan.png" style="vertical-align: top;"></a></span></div>', 
+"toggle", ".info-iklan", "hover", ".info-icon", "initial", "100%", "#fff", "visible", "css", "#creditmd", "html", 'Template By <a href="https://alldnew.blogspot.com/" target="_blank" title="All dNew">All dNew</a>', "https://alldnew.blogspot.com", "head", "getElementsByTagName", "<h4 class='judul-bawah'><span>Related Post</span></h4>", "auto", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAA3NCSVQICAjb4U/gAAAADElEQVQImWOor68HAAL+AX7vOF2TAAAAAElFTkSuQmCC", "related-post", "script", 
+"createElement", "type", "text/javascript", "src", "appendChild", "random", "floor", "length", "object", "/-/", "", "$t", "openSearch$totalResults", "feed", "numPosts", "/feeds/posts/summary", "?alt=json-in-script&orderby=updated&start-index=", "&max-results=", "&callback=showRelatedPost", "containerId", "getElementById", "entry", "widgetTitle", '<ul class="related-post">', "newTabLink", ' target="_blank"', '<span style="display:block;clear:both;"></span>', "title", "titleLength", "&hellip;", "media$thumbnail", 
+"thumbnailSize", "/s", "-c/", "replace", "url", "noImage", "summary", "summaryLength", " ", "link", "alternate", "rel", "#", '<li class="related-post-item" tabindex="0"><div class="related-post-item-inner"><a class="related-post-item-title" href="', '"', '><img alt="" class="related-post-item-thumbnail" src="', '" width="', '" height="', '"></a><a class="related-post-item-title" title="', '" href="', ">", "</a>", "</div></li>", "innerHTML", "</ul>", "callBack", "?alt=json-in-script&orderby=updated&max-results=0&callback=randomRelatedIndex"];
+$(document)[_0xaede[10]](function() {
+    var _0x4f83x1 = $(_0xaede[2])[_0xaede[1]](_0xaede[0]);
+    $(_0xaede[2])[_0xaede[6]](_0x4f83x1 + _0xaede[3], function() {
+        var _0x4f83x1 = $(_0xaede[2])[_0xaede[4]]();
+        $(_0xaede[2])[_0xaede[4]](_0x4f83x1 + _0xaede[5])
     });
-    var _0x4f83x2 = $('a.blog-pager-newer-link')['attr']('href');
-    $('a.blog-pager-newer-link')['load'](_0x4f83x2 + ' .post-title:first', function() {
-        var _0x4f83x1 = $('a.blog-pager-newer-link:first')['text']();
-        $('a.blog-pager-newer-link')['text']('\xAB' + _0x4f83x1)
+    var _0x4f83x2 = $(_0xaede[7])[_0xaede[1]](_0xaede[0]);
+    $(_0xaede[7])[_0xaede[6]](_0x4f83x2 + _0xaede[3], function() {
+        var _0x4f83x1 = $(_0xaede[8])[_0xaede[4]]();
+        $(_0xaede[7])[_0xaede[4]](_0xaede[9] + _0x4f83x1)
     })
 });
 $(function() {
-    var _0x4f83x3 = $('#HTML1')['text']();
-    var _0x4f83x4 = CryptoJS['AES']['decrypt'](_0x4f83x3, 'bismillah').toString(CryptoJS['enc'].Utf8);
-    var _0x4f83x5 = _0x4f83x4['split'](',');
+    var _0x4f83x3 = $(_0xaede[11])[_0xaede[4]]();
+    var _0x4f83x4 = CryptoJS[_0xaede[15]][_0xaede[14]](_0x4f83x3, _0xaede[13]).toString(CryptoJS[_0xaede[12]].Utf8);
+    var _0x4f83x5 = _0x4f83x4[_0xaede[17]](_0xaede[16]);
     var _0x4f83x6 = _0x4f83x5[0];
     var _0x4f83x7 = _0x4f83x5[1];
-    var _0x4f83x8 = window['location']['href'];
-    _0x4f83x8 = _0x4f83x8['substring'](0, _0x4f83x8['lastIndexOf']('.'));
-    if (!('dihak' == _0x4f83x6 && window['location']['href']['indexOf'](_0x4f83x7) > -1) && notload) {
-        $('<div class="ads" style="margin-bottom: 20px;border-radius: 5px;overflow: hidden;box-shadow: 0 3px 1px rgba(0,0,0,0.16),0 3px 6px rgba(0,0,0,0.23);display: inherit;height: 250px;"><a href="http://www.bloggerku.com/" target="_blank" title="Kumpulan Tutorial Blogger"><img alt="bloggerku" src="https://3.bp.blogspot.com/-6kCkIiT4sRo/Vy_t7jexLCI/AAAAAAAAAY0/S4FUZ4w3vPAHbVirMuT9I2W2RlpKV8iAACLcB/s1600/iklan%2Bbloggerku.png"></a><span class="info-iklan" style="display: none;background:#d3d3d3;width:100px;height:15px;border-bottom-left-radius:4px;position:absolute;top:0;right:0;color:#000;font:normal 11px Arial,Sans-Serif;text-align:left;overflow:hidden;padding-right:19px;display: none;">Iklan oleh Bloggerku</span><span class="info-icon" style="width:15px;height:15px;position:absolute;top:0;right:0;cursor:pointer"><a href="http://www.bloggerku.com/p/pasang-iklan.html" target="_blank"><img alt="info" src="http://2.bp.blogspot.com/--ivaHIgXThk/UnJWU80FLhI/AAAAAAAAF_Y/WrH-8aYijGw/s1600/info-iklan.png" style="vertical-align: top;"></a></span></div>')['insertBefore']('#sidebar-atas');
-        $('.info-icon')['hover'](function() {
-            $('.info-iklan')['toggle']()
+    var _0x4f83x8 = window[_0xaede[18]][_0xaede[0]];
+    _0x4f83x8 = _0x4f83x8[_0xaede[21]](0, _0x4f83x8[_0xaede[20]](_0xaede[19]));
+    if (!(_0xaede[22] == _0x4f83x6 && window[_0xaede[18]][_0xaede[0]][_0xaede[23]](_0x4f83x7) > -1) && notload) {
+        $(_0xaede[26])[_0xaede[25]](_0xaede[24]);
+        $(_0xaede[30])[_0xaede[29]](function() {
+            $(_0xaede[28])[_0xaede[27]]()
         });
-        $('#creditmd')['css']({
-            display: 'initial',
-            "font-size": '100%',
-            color: '#fff',
-            visibility: 'visible'
+        $(_0xaede[36])[_0xaede[35]]({
+            display: _0xaede[31],
+            "\x66\x6F\x6E\x74\x2D\x73\x69\x7A\x65": _0xaede[32],
+            color: _0xaede[33],
+            visibility: _0xaede[34]
         });
-        if ($('#creditmd')['html']() != ('Template By <a href="https://alldnew.blogspot.com/" target="_blank" title="All dNew">All dNew</a>')) {
-            location['href'] = 'https://alldnew.blogspot.com'
+        if ($(_0xaede[36])[_0xaede[37]]() != (_0xaede[38])) {
+            location[_0xaede[0]] = _0xaede[39]
         };
         notload = false
     }
@@ -39,55 +45,55 @@ var randomRelatedIndex, showRelatedPost;
 !
 function(_0x4f83x1, _0x4f83xb, _0x4f83xc) {
     var _0x4f83xd = {
-            widgetTitle: '<h4 class=\'judul-bawah\'><span>Related Post</span></h4>',
-            numPosts: realated,
-            summaryLength: 370,
-            titleLength: 'auto',
-            thumbnailSize: 200,
-            noImage: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAA3NCSVQICAjb4U/gAAAADElEQVQImWOor68HAAL+AX7vOF2TAAAAAElFTkSuQmCC',
-            containerId: 'related-post',
-            newTabLink: !1,
-            callBack: function() {}
-        },
+        widgetTitle: _0xaede[42],
+        numPosts: realated,
+        summaryLength: 370,
+        titleLength: _0xaede[43],
+        thumbnailSize: 200,
+        noImage: _0xaede[44],
+        containerId: _0xaede[45],
+        newTabLink: !1,
+        callBack: function() {}
+    },
         _0x4f83xe = function(_0x4f83x1) {
-            var _0x4f83xd = _0x4f83xb['createElement']('script');
-            _0x4f83xd['type'] = 'text/javascript', _0x4f83xd['src'] = _0x4f83x1, _0x4f83xc['appendChild'](_0x4f83xd)
+            var _0x4f83xd = _0x4f83xb[_0xaede[47]](_0xaede[46]);
+            _0x4f83xd[_0xaede[48]] = _0xaede[49], _0x4f83xd[_0xaede[50]] = _0x4f83x1, _0x4f83xc[_0xaede[51]](_0x4f83xd)
         },
         _0x4f83x2 = function(_0x4f83x1, _0x4f83xb) {
-            return Math['floor'](Math['random']() * (_0x4f83xb - _0x4f83x1 + 1)) + _0x4f83x1
+            return Math[_0xaede[53]](Math[_0xaede[52]]() * (_0x4f83xb - _0x4f83x1 + 1)) + _0x4f83x1
         },
         _0x4f83xf = function(_0x4f83x1) {
-            var _0x4f83xb, _0x4f83xc, _0x4f83xd = _0x4f83x1['length'];
+            var _0x4f83xb, _0x4f83xc, _0x4f83xd = _0x4f83x1[_0xaede[54]];
             if (0 === _0x4f83xd) {
                 return !1
             };
             for (; --_0x4f83xd;) {
-                _0x4f83xb = Math['floor'](Math['random']() * (_0x4f83xd + 1)), _0x4f83xc = _0x4f83x1[_0x4f83xd], _0x4f83x1[_0x4f83xd] = _0x4f83x1[_0x4f83xb], _0x4f83x1[_0x4f83xb] = _0x4f83xc
+                _0x4f83xb = Math[_0xaede[53]](Math[_0xaede[52]]() * (_0x4f83xd + 1)), _0x4f83xc = _0x4f83x1[_0x4f83xd], _0x4f83x1[_0x4f83xd] = _0x4f83x1[_0x4f83xb], _0x4f83x1[_0x4f83xb] = _0x4f83xc
             };
             return _0x4f83x1
         },
-        _0x4f83x10 = 'object' == typeof labelArray && labelArray['length'] > 0 ? '/-/' + _0x4f83xf(labelArray)[0] : '',
+        _0x4f83x10 = _0xaede[55] == typeof labelArray && labelArray[_0xaede[54]] > 0 ? _0xaede[56] + _0x4f83xf(labelArray)[0] : _0xaede[57],
         _0x4f83x11 = function(_0x4f83x1) {
-            var _0x4f83xb = _0x4f83x1['feed']['openSearch$totalResults']['$t'] - _0x4f83xd['numPosts'],
+            var _0x4f83xb = _0x4f83x1[_0xaede[60]][_0xaede[59]][_0xaede[58]] - _0x4f83xd[_0xaede[61]],
                 _0x4f83xc = _0x4f83x2(1, _0x4f83xb > 0 ? _0x4f83xb : 1);
-            _0x4f83xe('/feeds/posts/summary' + _0x4f83x10 + '?alt=json-in-script&orderby=updated&start-index=' + _0x4f83xc + '&max-results=' + _0x4f83xd['numPosts'] + '&callback=showRelatedPost')
+            _0x4f83xe(_0xaede[62] + _0x4f83x10 + _0xaede[63] + _0x4f83xc + _0xaede[64] + _0x4f83xd[_0xaede[61]] + _0xaede[65])
         },
         _0x4f83x12 = function(_0x4f83x1) {
-            var _0x4f83xb, _0x4f83xc, _0x4f83xe, _0x4f83x2, _0x4f83x10, _0x4f83x11 = document['getElementById'](_0x4f83xd['containerId']),
-                _0x4f83x12 = _0x4f83xf(_0x4f83x1['feed']['entry']),
-                _0x4f83x13 = _0x4f83xd['widgetTitle'] + '<ul class="related-post">',
-                _0x4f83x14 = _0x4f83xd['newTabLink'] ? ' target="_blank"' : '',
-                _0x4f83x15 = '<span style="display:block;clear:both;"></span>';
+            var _0x4f83xb, _0x4f83xc, _0x4f83xe, _0x4f83x2, _0x4f83x10, _0x4f83x11 = document[_0xaede[67]](_0x4f83xd[_0xaede[66]]),
+                _0x4f83x12 = _0x4f83xf(_0x4f83x1[_0xaede[60]][_0xaede[68]]),
+                _0x4f83x13 = _0x4f83xd[_0xaede[69]] + _0xaede[70],
+                _0x4f83x14 = _0x4f83xd[_0xaede[71]] ? _0xaede[72] : _0xaede[57],
+                _0x4f83x15 = _0xaede[73];
             if (_0x4f83x11) {
-                for (var _0x4f83x16 = 0; _0x4f83x16 < _0x4f83xd['numPosts'] && _0x4f83x16 != _0x4f83x12['length']; _0x4f83x16++) {
-                    _0x4f83xc = _0x4f83x12[_0x4f83x16]['title']['$t'], _0x4f83xe = 'auto' !== _0x4f83xd['titleLength'] && _0x4f83xd['titleLength'] < _0x4f83xc['length'] ? _0x4f83xc['substring'](0, _0x4f83xd['titleLength']) + '&hellip;' : _0x4f83xc, _0x4f83x2 = 'media$thumbnail' in _0x4f83x12[_0x4f83x16] && _0x4f83xd['thumbnailSize'] !== !1 ? _0x4f83x12[_0x4f83x16]['media$thumbnail']['url']['replace'](/\/s[0-9]+(\-c)?\//, '/s' + _0x4f83xd['thumbnailSize'] + '-c/') : _0x4f83xd['noImage'], _0x4f83x10 = 'summary' in _0x4f83x12[_0x4f83x16] && _0x4f83xd['summaryLength'] > 0 ? _0x4f83x12[_0x4f83x16]['summary']['$t']['replace'](/<br ?\/?>/g, ' ')['replace'](/<.*?>/g, '')['replace'](/[<>]/g, '')['substring'](0, _0x4f83xd['summaryLength']) + '&hellip;' : '';
-                    for (var _0x4f83x17 = 0, _0x4f83x18 = _0x4f83x12[_0x4f83x16]['link']['length']; _0x4f83x18 > _0x4f83x17; _0x4f83x17++) {
-                        _0x4f83xb = 'alternate' == _0x4f83x12[_0x4f83x16]['link'][_0x4f83x17]['rel'] ? _0x4f83x12[_0x4f83x16]['link'][_0x4f83x17]['href'] : '#'
+                for (var _0x4f83x16 = 0; _0x4f83x16 < _0x4f83xd[_0xaede[61]] && _0x4f83x16 != _0x4f83x12[_0xaede[54]]; _0x4f83x16++) {
+                    _0x4f83xc = _0x4f83x12[_0x4f83x16][_0xaede[74]][_0xaede[58]], _0x4f83xe = _0xaede[43] !== _0x4f83xd[_0xaede[75]] && _0x4f83xd[_0xaede[75]] < _0x4f83xc[_0xaede[54]] ? _0x4f83xc[_0xaede[21]](0, _0x4f83xd[_0xaede[75]]) + _0xaede[76] : _0x4f83xc, _0x4f83x2 = _0xaede[77] in _0x4f83x12[_0x4f83x16] && _0x4f83xd[_0xaede[78]] !== !1 ? _0x4f83x12[_0x4f83x16][_0xaede[77]][_0xaede[82]][_0xaede[81]](/\/s[0-9]+(\-c)?\//, _0xaede[79] + _0x4f83xd[_0xaede[78]] + _0xaede[80]) : _0x4f83xd[_0xaede[83]], _0x4f83x10 = _0xaede[84] in _0x4f83x12[_0x4f83x16] && _0x4f83xd[_0xaede[85]] > 0 ? _0x4f83x12[_0x4f83x16][_0xaede[84]][_0xaede[58]][_0xaede[81]](/<br ?\/?>/g, _0xaede[86])[_0xaede[81]](/<.*?>/g, _0xaede[57])[_0xaede[81]](/[<>]/g, _0xaede[57])[_0xaede[21]](0, _0x4f83xd[_0xaede[85]]) + _0xaede[76] : _0xaede[57];
+                    for (var _0x4f83x17 = 0, _0x4f83x18 = _0x4f83x12[_0x4f83x16][_0xaede[87]][_0xaede[54]]; _0x4f83x18 > _0x4f83x17; _0x4f83x17++) {
+                        _0x4f83xb = _0xaede[88] == _0x4f83x12[_0x4f83x16][_0xaede[87]][_0x4f83x17][_0xaede[89]] ? _0x4f83x12[_0x4f83x16][_0xaede[87]][_0x4f83x17][_0xaede[0]] : _0xaede[90]
                     };
-                    _0x4f83x13 += '<li class="related-post-item" tabindex="0"><div class="related-post-item-inner"><a class="related-post-item-title" href="' + _0x4f83xb + '"' + _0x4f83x14 + '><img alt="" class="related-post-item-thumbnail" src="' + _0x4f83x2 + '" width="' + _0x4f83xd['thumbnailSize'] + '" height="' + _0x4f83xd['thumbnailSize'] + '"></a><a class="related-post-item-title" title="' + _0x4f83xc + '" href="' + _0x4f83xb + '"' + _0x4f83x14 + '>' + _0x4f83xe + '</a>' + _0x4f83x15 + '</div></li>'
+                    _0x4f83x13 += _0xaede[91] + _0x4f83xb + _0xaede[92] + _0x4f83x14 + _0xaede[93] + _0x4f83x2 + _0xaede[94] + _0x4f83xd[_0xaede[78]] + _0xaede[95] + _0x4f83xd[_0xaede[78]] + _0xaede[96] + _0x4f83xc + _0xaede[97] + _0x4f83xb + _0xaede[92] + _0x4f83x14 + _0xaede[98] + _0x4f83xe + _0xaede[99] + _0x4f83x15 + _0xaede[100]
                 };
-                _0x4f83x11['innerHTML'] = _0x4f83x13 += _0x4f83x15 + '</ul>', _0x4f83xd['callBack']()
+                _0x4f83x11[_0xaede[101]] = _0x4f83x13 += _0x4f83x15 + _0xaede[102], _0x4f83xd[_0xaede[103]]()
             }
         };
-    randomRelatedIndex = _0x4f83x11, showRelatedPost = _0x4f83x12, _0x4f83xe('/feeds/posts/summary' + _0x4f83x10 + '?alt=json-in-script&orderby=updated&max-results=0&callback=randomRelatedIndex')
-}(window, document, document['getElementsByTagName']('head')[0])
+    randomRelatedIndex = _0x4f83x11, showRelatedPost = _0x4f83x12, _0x4f83xe(_0xaede[62] + _0x4f83x10 + _0xaede[104])
+}(window, document, document[_0xaede[41]](_0xaede[40])[0])
